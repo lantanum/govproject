cec56ecabdd8e3f2ff51fac89acb9b7e174e7e96
Magamomed <saparov-sultan2017@bk.ru>
2024-11-18 03:21:39 +0500

    Adding an authorization page

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,6 +1,7 @@
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # Другие маршруты...
+    path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
 ]
